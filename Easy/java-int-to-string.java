5d7ed8639bb791a001b56208f3048886df933692
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-01-18 20:09:59 -0300

    java-int-to-string

---
+++ Easy/java-int-to-string.java
@@ -0,0 +1,26 @@
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+   String s = Integer.toString(n);
+   //Escrever um valor Integer N
+   // E conveter para string
+
+
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
