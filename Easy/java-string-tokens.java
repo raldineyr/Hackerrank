712e4ea8ac789dbdcf799b51ad33de94527811ac
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-01-30 21:37:08 -0300

    Java-string-tokens

---
+++ Easy/java-string-tokens.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        
+    s = s.trim();
+    
+    if(s.length() == 0) {
+        System.out.println(0);
+        return;
+    }
+    
+      String [] words = s.split("[^a-zA-Z]+");
+      System.out.println(words.length);
+      for(String wordIn : words){
+          System.out.println(wordIn);
+      }
+        scan.close();
+    }
+}
